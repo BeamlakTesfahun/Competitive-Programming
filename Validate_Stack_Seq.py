6d21ac420bf67cec41f4bef10261d53ae0412313
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-10-11 14:59:14 -0600

    Create Validate_Stack_Seq.py

---
+++ Validate_Stack_Seq.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        N = len(popped)
+        i = 0 # pointer of pop
+        for p in pushed:
+            stack.append(p)
+            while stack and i<N and stack[-1] == popped[i]:
+                i +=1 
+                stack.pop()
+        return stack == []
