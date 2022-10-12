1e7a7361acf458b9641b3130cd563cf7f218ea6a
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-10-12 05:10:31 -0600

    Create Middle_of_Linked_List.py

---
+++ Middle_of_Linked_List.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_pointer = head
+        fast_pointer = head 
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+        return slow_pointer
