f19d21c25ccd4e93ae65646a7f2c1eb18edfb62a
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-10-12 06:27:55 -0600

    Create Delete_Node_in_Linked_List.py

---
+++ Delete_Node_in_Linked_List.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        temp = node.next
+        node.val = temp.val
+        node.next = temp.next
