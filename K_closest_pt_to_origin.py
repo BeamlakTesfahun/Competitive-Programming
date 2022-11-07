8b0becd0b07abd1a36f9bf24face0099787d8092
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-11-07 06:07:54 -0700

    Update K_closest_pt_to_origin.py

--- K_closest_pt_to_origin.py
+++ K_closest_pt_to_origin.py
@@ -1,13 +1,19 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        minHeap = []
-        for x,y in points:
-            dist = x**2 + y**2
-            minHeap.append([dist,x,y])
-        heapq.heapify(minHeap)
-        result = []
-        while k> 0:
-            dist, x, y = heapq.heappop(minHeap)
-            result.append([x,y])
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result = [] 
+        answer = []
+        for x, y in points:
+            dist = x ** 2 + y ** 2
+            result.append([dist, x, y])
+        result = sorted(result, key = lambda x: x[0], reverse = True)
+        print(result)
+        while k > 0:
+            dist, x, y = result.pop()
+            answer.append([x,y])
             k -= 1
-        return result
+        return answer
