46d970a587dcc1f12b4516809144a8eeaa359e66
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-11-07 05:51:52 -0700

    Update Sort_colors.py

--- Sort_colors.py
+++ Sort_colors.py
@@ -1,19 +1,11 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        current = 0
-        right = len(nums) - 1
-        
-        while current<= right:
-            if nums[current] == 0:
-                nums[left] , nums[current] = nums[current] , nums[left]
-                left += 1
-                current += 1
-            elif nums[current] == 2:
-                nums[current], nums[right] = nums[right], nums[current]
-                right -= 1
-            else:
-                current += 1
+        for i in range(1, len(nums)):
+            j = i
+            while nums[j - 1] > nums[j] and j > 0:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                j -= 1
