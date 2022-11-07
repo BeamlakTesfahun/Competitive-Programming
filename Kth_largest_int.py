41d2b8bf4d490c60fa3577bc3da857743e14cab1
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-11-07 07:10:13 -0700

    Update Kth_largest_int.py

--- Kth_largest_int.py
+++ Kth_largest_int.py
@@ -1,8 +1,4 @@
-class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        maxHeap = [-int(n) for n in nums]
-        heapq.heapify(maxHeap)
-        while k > 1:
-            heapq.heappop(maxHeap)
-            k -= 1
-        return str(-maxHeap[0])
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        sorted_arr = sorted(nums, key= int,reverse = True)
+        return sorted_arr[k-1]
